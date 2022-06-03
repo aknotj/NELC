b4e2a3fd5aaea62df4500e12dd887d939bd166b3
aknotj <akaneotsuji08@gmail.com>
2022-06-03 04:47:53 +0000

    Created messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
     end
 
     resources :rooms, only: [:create, :show, :index]
+    resources :messages, only: [:create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
