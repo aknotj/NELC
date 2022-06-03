25eee29ef8cbf31820f39e4bd51294e4779fcd62
aknotj <akaneotsuji08@gmail.com>
2022-06-03 03:35:57 +0000

    Added guest login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         
+  #ゲストユーザー情報を探し、存在しなければ作成
+  def self.guest
+    find_or_create_by!(email: 'guest@nelc.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.name = "Guest User"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'home/top'
-    get 'home/home'
-  end
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
-  
+
   devise_for :users, skip: [:passwords], controllers: {
     registrations: 'public/registrations',
     sessions: 'public/sessions'
   }
-  
+  devise_scope :user do
+    post 'users/guest_sign_in' => "public/sessions#new_guest"
+  end
+
   scope module: :public do
     root to: "home#top"
     get "/home", to: "home#home"
     resources :users, only: [:index, :show, :edit, :update, :destroy]
-  
+
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
