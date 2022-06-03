440476183202e42bdc64fcde5a44849058b56f66
aknotj <akaneotsuji08@gmail.com>
2022-06-03 00:35:43 +0000

    Added columns to User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_233023) do
+ActiveRecord::Schema.define(version: 2022_06_03_002611) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 2022_06_02_233023) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", null: false
+    t.string "name_jap"
+    t.integer "gender"
+    t.string "native_language"
+    t.string "learning_language"
+    t.text "introduction"
+    t.boolean "is_deactivated", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
