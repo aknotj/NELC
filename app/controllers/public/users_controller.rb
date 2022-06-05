02e7afe6985a008a40d00040a3425e60b5bbd0f2
aknotj <akaneotsuji08@gmail.com>
2022-06-05 13:55:56 +0000

    Added views for users edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def index
     @users = User.all
   end
@@ -10,13 +12,32 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    end
   end
 
   def posts
     @user = User.find(params[:id])
     @posts = @user.posts
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :name_jap, :gender, :native_language, :learning_language, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(@user)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  enum gender: {male: 0, female: 1, other: 2}
+
+
   has_one_attached :profile_image
   has_many :posts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed, dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower, dependent: :destroy
-  
+
   #ゲストユーザー情報を探し、存在しなければ作成
   def self.guest
     find_or_create_by!(email: 'guest@nelc.com') do |user|
