dfffa6964f21822aea8b5748c325e9419b4277f2
aknotj <akaneotsuji08@gmail.com>
2022-06-05 11:47:25 +0000

    Added views for users show

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,11 +3,19 @@ class Public::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @users = User.all.limit(5) #Reminder: change User.all to @user.friends
+    @posts = @user.posts.limit(3)
   end
 
   def edit
   end
-  
+
   def update
+  end
+
+  def posts
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 end
