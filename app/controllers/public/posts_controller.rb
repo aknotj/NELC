f5138872b2e4f411d0b6816837c1796dcdbdd60a
aknotj <akaneotsuji08@gmail.com>
2022-06-06 05:13:34 +0000

    Created posts new/create

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,23 +1,39 @@
 class Public::PostsController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy, :drafts]
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if  @post.update(post_params)
+      redirect_to blog_post_path(@post.blog_id, @post)
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def drafts
   end
 
   def index
+    @posts = Post.all
   end
 
   def by_category
@@ -26,4 +42,17 @@ class Public::PostsController < ApplicationController
   def destroy
   end
 
+
+  protected
+
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user == current_user
+      redirect_to post_path(@post)
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :language)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :categories, through: :post_categories
+  has_many :post_categories, dependent: :destroy
+
+  validates :user_id, presence: true
+  validates :title, presence: true, length: {maximum: 50}
+  validates :body, presence: true, length: {maximum: 300}
+
+  enum language: {english: 0, japanese: 1, other: 2}
+
 end
