add99a1f53cac708b99588b543a972ccf7bc4c19
aknotj <akaneotsuji08@gmail.com>
2022-06-07 04:47:35 +0000

    Created bookmark actions and index

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,8 +1,20 @@
 class Public::BookmarksController < ApplicationController
-  
+
   def create
+    @post = Post.find(params[:post_id])
+    current_user.bookmarks.create(post_id: @post.id)
+    redirect_to request.referer
   end
 
   def destroy
-end
+    @post = Post.find(params[:post_id])
+    current_user.bookmarks.find_by(post_id: @post.id).destroy
+    redirect_to request.referer
+  end
+
+  def index
+    bookmarks = current_user.bookmarks.pluck(:post_id)
+    posts = Post.find(bookmarks)
+    @posts = Kaminari.paginate_array(posts).page(params[:page])
+  end
 end
