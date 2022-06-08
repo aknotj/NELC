541f39f519b06b3ae8ac2c4491bc1eaf8d46b484
aknotj <akaneotsuji08@gmail.com>
2022-06-08 08:54:07 +0000

    Created posts by category views and side bar

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,18 @@
 class Category < ApplicationRecord
   has_many :post_categories, dependent: :destroy
-  has_many :posts, through: :post_categories
+  has_many :posts, ->{order('created_at desc')}, through: :post_categories
 
   validates :name, presence: true, length:{maximum: 50}
+
+  #登録post数が多い順番に表示
+  def self.order_by_posts
+    Category.joins(:post_categories).group('category_id').order('count(post_id) desc')
+  end
+  
+  def self.tagged_by(user)
+    posts = user.posts.pluck(:id)
+    categories = PostCategory.where(post_id: posts).pluck(:category_id)
+    Category.where(id: categories)
+  end
+
 end
