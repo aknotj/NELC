b28d1037bcf613d4544a0f89f9ec70aec91120ee
aknotj <akaneotsuji08@gmail.com>
2022-06-09 07:44:18 +0000

    Created user and post keyword search

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,4 +1,15 @@
 class Public::SearchesController < ApplicationController
+
   def search
+    @model = params[:model]
+    @language = params[:language]
+    @name = params[:name]
+    @gender = params[:gender]
+    @content = params[:content]
+    if @model == "Users"
+      @records = User.search_for(@language, @name, @gender, @content)
+    elsif @model == "Posts"
+      @records = Post.search_for(@language, @content)
+    end
   end
 end
