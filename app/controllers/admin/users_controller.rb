a2a18a7afe6ee3f3551e85de32e730add1b5d435
aknotj <akaneotsuji08@gmail.com>
2022-06-09 22:48:20 +0000

    Modified admin user views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,11 @@
 class Admin::UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page]).per(6)
   end
   
   def update
