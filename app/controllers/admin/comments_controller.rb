2f9bfcd6e4235a201a2e7be2a04820de5406dd75
aknotj <akaneotsuji08@gmail.com>
2022-06-09 11:49:09 +0000

    Created admin actions and views to change display statuses

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,10 +1,24 @@
 class Admin::CommentsController < ApplicationController
   def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.all
   end
 
   def show
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to admin_post_comment_path(@comment)
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:is_deleted)
   end
 end
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,21 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to admin_post_path(@post)
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:is_deleted)
   end
 end
