9705a7d87a6a6e63f7a8275d8dc36a03155472ce
aknotj <akaneotsuji08@gmail.com>
2022-06-11 09:11:04 +0000

    Created admin reports views

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,7 +1,22 @@
 class Admin::ReportsController < ApplicationController
+
   def index
+    @reports = Report.page(params[:page])
   end
 
   def show
+    @report = Report.find(params[:id])
+    @report_note = @report.note
+  end
+
+  def update
+    @report = Report.find(params[:id])
+    @report.update(report_params)
+    redirect_to admin_report_path(@report)
+  end
+
+private
+  def report_params
+    params.require(:report).permit(:is_closed, :note)
   end
 end
