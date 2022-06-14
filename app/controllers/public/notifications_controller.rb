446747c17b8684f253e7ae560cdef20e59e0776d
aknotj <akaneotsuji08@gmail.com>
2022-06-14 06:18:08 +0000

    Ajax / bootstrap js notification delete button

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,5 +1,5 @@
 class Public::NotificationsController < ApplicationController
-  beore_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @notifications = current_user.passive_notifications.all
@@ -7,12 +7,10 @@ class Public::NotificationsController < ApplicationController
 
   def update
     @notification = Notification.find(params[:id]).update(is_checked: true)
-    redirect_to notifications_path
   end
 
   def update_all
     current_user.passive_notifications.update_all(is_checked: true)
-    redirect_to notifications_path
   end
 
 end
