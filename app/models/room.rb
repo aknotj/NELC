4e4dd86115858002d006b8fac3aaa6991a836cad
aknotj <akaneotsuji08@gmail.com>
2022-06-18 09:38:18 +0000

    Added new chat notification

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   has_many :entries, dependent: :destroy
   has_many :users, through: :entries
   has_many :messages, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
   def is_valid? #相手が存在しないルームを表示しない
     users.active.count == 2
@@ -14,6 +15,14 @@ class Room < ApplicationRecord
   def last_sent
     message_id = messages.pluck(:id).last
     Message.find_by(room_id: id, id: message_id)
+  end
+  
+  def read_all_messages(current_user)
+    current_user.passive_notifications.where(room_id: id).update(is_checked: true)
+  end
+
+  def unchecked_notifications(current_user)
+    current_user.passive_notifications.where(room_id: id).where(is_checked: false)
   end
 
 end
