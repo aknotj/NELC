f4a2d27e6a54524bbc91f1e0672af4f922a29801
aknotj <akaneotsuji08@gmail.com>
2022-06-21 06:39:05 +0000

    Removed ajax from update all notification btn/action

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -11,6 +11,7 @@ class Public::NotificationsController < ApplicationController
 
   def update_all
     current_user.passive_notifications.update_all(is_checked: true)
+    redirect_to notifications_path
   end
 
 end
